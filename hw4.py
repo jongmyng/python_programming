b4c27d6bc2f7bebb40b977c424b4cd4dcebf5b05
jongmyng <163854324+jongmyng@users.noreply.github.com>
2024-04-11 19:29:12 +0900

    Add files via upload

--- hw4.py
+++ hw4.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[5]:
 
 
-input('Input his/her name:')
+def rep_char(c,n):
+    print(c*n)
+    
+    
+def draw_line_string(word):
+    if len('hello' + word) > len('Welcome to seoul'):
+        rep_char('-', 2*len('hello' + word))
+    else: rep_char('-', 3+len('Welcome to seoul'))
+    
+    print(f' Hello, {word}' '\n'
+          '','Welcome to Seoul.')
+    
+    if len('hello' + word) > len('Welcome to seoul'):
+        rep_char('-', 2*len('hello' + word))
+    else: rep_char('-', 3+len('Welcome to seoul'))
+
+    
+
+msg1= input('input his/her name:')
+
+draw_line_string(msg1)            
+    
 
 
 # In[ ]:
